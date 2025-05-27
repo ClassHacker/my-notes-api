28237e6c1c179a9aa65fca49282a20cb6ee3cd5e
ClassHacker <ajaydayma1401@gmail.com>
2025-05-27 10:59:53 +0530

    Update: Updated DemoSpringBootApplication.java

--- src/main/java/com/example/demo/DemoSpringBootApplication.java
+++ src/main/java/com/example/demo/DemoSpringBootApplication.java
@@ -2,26 +2,25 @@ package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
-import com.example.demo.dynamo.factory.DynamoDBClientFactory;
+import com.example.demo.db.factory.ClientFactory;
+import org.springframework.context.annotation.Profile;
 
-//@SpringBootApplication
-@ComponentScan
-@SpringBootConfiguration
-@EnableAutoConfiguration
+@SpringBootApplication
 public class DemoSpringBootApplication {
 	
-	@Autowired DynamoDBClientFactory dynamoDBClientFactory;
+	@Autowired
+	ClientFactory dynamoDBClientFactory;
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoSpringBootApplication.class, args);
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(DemoSpringBootApplication.class, args);
 	}
-	
+
+	@Profile("dynamo-db")
 	@Bean
 	DynamoDB getDynamoDB() {
 		return dynamoDBClientFactory.getDynamoDB();
